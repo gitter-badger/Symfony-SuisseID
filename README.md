f8a97ee6a366d5b2635674ad0c30bdd1fa1e0c87
Dominik Müller <d.mueller@iwf.ch>
2015-12-24 13:12:53 +0100

    Optimized locale listener and updated README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Symfony Base 2.8
 
 A Symfony project created on December 24, 2015, 7:35 am.
 
-This repository was created as a blueprint for Symfony applications.
+This repository was created as a blueprint for multilingual Symfony applications.
 
 Feel free to fork this repo.
 
--- src/AppBundle/EventListener/LocaleListener.php
+++ src/AppBundle/EventListener/LocaleListener.php
@@ -28,16 +28,18 @@ class LocaleListener implements EventSubscriberInterface
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-        if (!$request->hasPreviousSession()) {
+
+        if (!$request->hasPreviousSession()) { // First visit
+            $request->getSession()->set('_locale', $request->getPreferredLanguage());
+            $request->setLocale($request->getPreferredLanguage());
             return;
         }
 
+        $locale = $request->attributes->get('_locale');
+
         // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $request->getPreferredLanguage()));
+        if (empty($locale)) { // No _locale supplied
+            $request->setLocale($request->getSession()->get('_locale'));
         }
     }
 
