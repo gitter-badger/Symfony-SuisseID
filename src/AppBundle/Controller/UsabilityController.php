0f411ff17dd4fb4ef0948e106926f1d7439d86a9
Dominik Müller <d.mueller@iwf.ch>
2015-12-24 13:26:54 +0100

    Added usability feature

---
+++ src/AppBundle/Controller/UsabilityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UsabilityController
+ *
+ * I added this class for some user-friendly features like: remove trailing slashes etc.
+ *
+ * @package AppBundle\Controller
+ */
+class UsabilityController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
+    }
+}
