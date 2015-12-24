8e6f0a9b55c99d74f9ea00100925159bf3f43723
Dominik Müller <d.mueller@iwf.ch>
2015-12-24 15:04:03 +0100

    Fixed multilingual routing issue

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class AppController
@@ -25,5 +26,17 @@ class AppController extends Controller
                 'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
             )
         );
+    }
+
+    /**
+     * @Route("/test", name="test_route")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function testAction(Request $request)
+    {
+        return new Response($request->getLocale());
     }
 }
--- src/AppBundle/Controller/UsabilityController.php
+++ src/AppBundle/Controller/UsabilityController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class UsabilityController
@@ -21,11 +22,52 @@ class UsabilityController extends Controller
      */
     public function removeTrailingSlashAction(Request $request)
     {
-        $pathInfo = $request->getPathInfo();
+        $pathInfo   = $request->getPathInfo();
         $requestUri = $request->getRequestUri();
 
         $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
 
         return $this->redirect($url, 301);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function localeRedirectAction(Request $request)
+    {
+        $requestUri  = $request->getRequestUri();
+        $explodedUri = explode('/', $requestUri);
+        $doubleSlash = substr($requestUri, 0, 2) === '//';
+
+        if (!$doubleSlash){ // does not start with 2 slashes
+
+            if (strlen($explodedUri[1]) !== 2){ // no locale supplied
+                $matchingRoute = $this->get('router')->match("/".$requestUri);
+            } else{
+
+                // 404
+                throw $this->createNotFoundException();
+            }
+        } else{
+
+            $requestUri = substr($requestUri, 2); // Removed first 2 slashes
+
+            $locale = (empty($request->getLocale())) ? $request->getDefaultLocale() : mb_substr($request->getLocale(),
+                0, 2
+            );
+
+            $requestUri = '/'.$locale.'/'.$requestUri;
+
+            $matchingRoute = $this->get('router')->match($requestUri);
+        }
+
+
+        if ($matchingRoute['_controller'] === 'AppBundle\Controller\UsabilityController::localeRedirectAction'){
+            throw $this->createNotFoundException();
+        }
+
+        return $this->forward($matchingRoute['_controller']);
     }
 }
